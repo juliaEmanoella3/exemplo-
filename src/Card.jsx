c1c20b94ba37842fcffe3277d2b0a4b84d4b6c73
Julia Emanoella <julia.emanoella.nk@gmail.com>
2025-09-25 17:52:45 -0300

    add comp

---
+++ src/Card.jsx
@@ -0,0 +1,21 @@
+export default function Card(props) {
+    return (
+
+        <div class="card">
+            <p>{props.texto}</p>
+            
+
+
+        </div>
+
+    )
+}
+
+
+function CardOutro() {
+    return (
+        <>
+            Isso é um Card
+        </>
+    )
+}
